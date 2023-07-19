a59f686a9b0af5f8ccdcbaab7324189dd242878d
Ansar Nur Jamas <jamasguitarist@gmail.com>
2023-07-19 22:46:12 +0700

    penerapan layout, sidebar dan merevisi beberapa bug

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -1,37 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    {{-- <title> {{ $pageTitle }} <title> --}}
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <!-- Google Web Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Roboto:wght@500;700&display=swap" rel="stylesheet">
-
-    <!-- Icon Font Stylesheet -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-
-    <!-- Libraries Stylesheet -->
-    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="{{asset('public/fontawesome/css/all.css')}}">
-
-    @vite(['/resources/css/style.css'])
-    @vite(['/resources/css/bootstrap.min.css'])
-    @vite(['/resources/sass/app.scss'])
-    @vite(['/lib/owlcarousel/assets/owl.carousel.min.css'])
-    @vite(['/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css'])
-
-</head>
-
-<body>
-    <div class="container-fluid position-relative d-flex p-0">
+@extends('layouts.app')
+@section('content')
+<div class="container-fluid position-relative d-flex p-0">
         <!-- Sidebar Start -->
         <div class="sidebar pe-4 pb-3">
             <nav class="navbar bg-secondary navbar-dark">
@@ -46,7 +15,6 @@
                     <a href="{{route('customer.index')}}" class="nav-item nav-link active"><i class="fa fa-user-friends me-2"></i>Pelanggan</a>
                     <a href="{{route('order')}}" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Penjualan</a>
                     <a href="" class="nav-item nav-link"><i class="fa fa-cash-register me-2"></i>Titik Penjualan</a>
-                    <a href="" class="nav-item nav-link"><i class="fa fa-cog me-2"></i>Setting</a>
                     <a href="{{ route('logout') }}" class="nav-item nav-link"><i class="fa fa-sign-out-alt me-2"
                      onclick="event.preventDefault();
                         document.getElementById('logout-form').submit();">
@@ -63,8 +31,6 @@
 
 
         <div class="content">
-
-            <!-- Navbar Start -->
             <nav class="navbar navbar-expand bg-secondary navbar-dark sticky-top px-4 py-0">
                 <a href="index.html" class="navbar-brand d-flex d-lg-none me-4">
                     <h2 class="text-primary mb-0"><i class="fa fa-user-edit"></i></h2>
@@ -81,22 +47,17 @@
                     </div>
                 </div>
             </nav>
-            <!-- Navbar End -->
+
+            {{-- Title + Button  --}}
             <div class="d-flex align-items-center justify-content-between mb-4">
                 <h4 class="ms-4 mt-4">Manajemen Pelanggan</h4>
-                <a href="" class="btn btn-success me-4 mt-4"><i class="fas fa-plus"></i> Add New Customer</a>
+                <a href="{{route('customer.create')}}" class="me-4 mt-4 btn btn-success"><i class="fas fa-plus"></i> Tambahkan Pelanggan</a>
             </div>
-
-
-
+            {{-- End Title + Button --}}
 
              <!-- Recent Sales Start -->
              <div class="container-fluid pt-4 px-4">
                 <div class="bg-secondary text-center rounded p-4">
-                    <div class="d-flex align-items-center justify-content-between mb-4">
-                        <h6 class="mb-0">Manajemen Pelanggan </h6>
-                        <a href="">Lihat Semua</a>
-                    </div>
                     <div class="table-responsive">
                         <table class="table text-start align-middle table-bordered table-hover mb-0">
                             <thead>
@@ -120,7 +81,10 @@
                                     <td>089666530317</td>
                                     <td>sidoarjo</td>
                                     <td>10 JANUARI 2023</td>
-                                    <td><a style="background-color: rgba(53, 142, 224, 1)" class=" mx-3 btn btn-sm btn-dark far fa-edit " href=""></a> <a class=" btn btn-sm btn-primary" href=""><i class="fa fa-trash"></i></td>
+                                    <td>
+                                        <a style="background-color: rgba(53, 142, 224, 1)" class=" mx-3 btn btn-sm btn-dark far fa-edit" href=""></a>
+                                        <a class=" btn btn-sm btn-primary" href=""><i class="fa fa-trash"></i>
+                                    </td>
 
                                 </tr>
                                 <tr>
@@ -167,19 +131,4 @@
                         </div> --}}
                         <!-- Footer End -->
                     </div>
-
-<!-- Template Javascript -->
-{{-- <script src="js/main.js"></script> --}}
-<script src="{{ asset('resources/js/jquery.js') }}"></script>
-<script src="{{ asset('resources/js/fa.js') }}"></script>
-<script src="{{ asset('resources/js/main.js') }}"></script>
-<script src="{{ asset('lib/chart/chart.min.js') }}"></script>
-<script src="{{ asset('lib/easing/easing.min.js') }}"></script>
-<script src="{{ asset('lib/waypoints/waypoints.min.js') }}"></script>
-<script src="{{ asset('lib/owlcarousel/owl.carousel.min.js') }}"></script>
-<script src="{{ asset('lib/tempusdominus/js/moment.min.js') }}"></script>
-<script src="{{ asset('lib/tempusdominus/js/moment-timezone.min.js') }}"></script>
-<script src="{{ asset('lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js') }}"></script>
-
-</body>
-</html>
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,36 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    {{-- <title> {{ $pageTitle }} <title> --}}
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <!-- Google Web Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Roboto:wght@500;700&display=swap" rel="stylesheet">
-
-    <!-- Icon Font Stylesheet -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-
-    <!-- Libraries Stylesheet -->
-    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="{{asset('public/fontawesome/css/all.css')}}">
-
-    @vite(['/resources/css/style.css'])
-    @vite(['/resources/css/bootstrap.min.css'])
-    @vite(['/resources/sass/app.scss'])
-    @vite(['/lib/owlcarousel/assets/owl.carousel.min.css'])
-    @vite(['/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css'])
-
-</head>
-
-<body>
+@extends('layouts.app')
+@section('content')
     <div class="container-fluid position-relative d-flex p-0">
         <!-- Sidebar Start -->
         <div class="sidebar pe-4 pb-3">
@@ -46,7 +15,6 @@
                     <a href="{{route('customer.index')}}" class="nav-item nav-link"><i class="fa fa-user-friends me-2"></i>Pelanggan</a>
                     <a href="{{route('order')}}" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Penjualan</a>
                     <a href="" class="nav-item nav-link"><i class="fa fa-cash-register me-2"></i>Titik Penjualan</a>
-                    <a href="" class="nav-item nav-link"><i class="fa fa-cog me-2"></i>Setting</a>
                     <a href="{{ route('logout') }}" class="nav-item nav-link"><i class="fa fa-sign-out-alt me-2"
                      onclick="event.preventDefault();
                         document.getElementById('logout-form').submit();">
@@ -59,12 +27,11 @@
                 </div>
             </nav>
         </div>
+
         <!-- Sidebar End -->
 
 
         <div class="content">
-
-            <!-- Navbar Start -->
             <nav class="navbar navbar-expand bg-secondary navbar-dark sticky-top px-4 py-0">
                 <a href="index.html" class="navbar-brand d-flex d-lg-none me-4">
                     <h2 class="text-primary mb-0"><i class="fa fa-user-edit"></i></h2>
@@ -81,14 +48,11 @@
                     </div>
                 </div>
             </nav>
-            <!-- Navbar End -->
-
-
             <!-- Sale & Revenue Start -->
             <div class="container-fluid pt-4 px-4">
                 <div class="row g-4">
                     <div class="col-sm-6 col-xl-3">
-                        <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
+                        <div class="bg-secondary rounded d-flex align-items-center p-4">
                             <i class="fa fa-shopping-cart fa-3x text-primary""></i>
                             <div class="ms-3">
                                 <p class="mb-2 text-primary">Total Produk</p>
@@ -97,7 +61,7 @@
                         </div>
                     </div>
                     <div class="col-sm-6 col-xl-3">
-                        <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
+                        <div class="bg-secondary rounded d-flex align-items-center p-4">
                             <i class="fa fa-chart-line fa-3x text-primary"></i>
                             <div class="ms-3">
                                 <p class="mb-2 text-primary">Total Penjualan</p>
@@ -106,7 +70,7 @@
                         </div>
                     </div>
                     <div class="col-sm-6 col-xl-3">
-                        <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
+                        <div class="bg-secondary rounded d-flex align-items-center p-4">
                             <i class="fa fa-dollar-sign fa-3x text-primary"></i>
                             <div class="ms-3">
                                 <p class="mb-2 text-primary">Total Pendapatan</p>
@@ -115,7 +79,7 @@
                         </div>
                     </div>
                     <div class="col-sm-6 col-xl-3">
-                        <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
+                        <div class="bg-secondary rounded d-flex align-items-center p-4">
                             <i class="fa fa-user-friends fa-3x text-primary"></i>
                             <div class="ms-3">
                                 <p class="mb-2 text-primary">Total Pelanggan</p>
@@ -124,7 +88,7 @@
                         </div>
                     </div>
                     <div class="col-sm-6 col-xl-3">
-                        <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
+                        <div class="bg-secondary rounded d-flex align-items-center p-4">
                             <i class="fa fa-calendar-day fa-3x text-primary"></i>
                             <div class="ms-3">
                                 <p class="mb-2 text-primary">Pendapatan Hari Ini</p>
@@ -323,19 +287,5 @@
                         </div>
                         <!-- Footer End -->
                     </div>
-
-<!-- Template Javascript -->
-{{-- <script src="js/main.js"></script> --}}
-<script src="{{ asset('resources/js/jquery.js') }}"></script>
-<script src="{{ asset('resources/js/fa.js') }}"></script>
-<script src="{{ asset('resources/js/main.js') }}"></script>
-<script src="{{ asset('lib/chart/chart.min.js') }}"></script>
-<script src="{{ asset('lib/easing/easing.min.js') }}"></script>
-<script src="{{ asset('lib/waypoints/waypoints.min.js') }}"></script>
-<script src="{{ asset('lib/owlcarousel/owl.carousel.min.js') }}"></script>
-<script src="{{ asset('lib/tempusdominus/js/moment.min.js') }}"></script>
-<script src="{{ asset('lib/tempusdominus/js/moment-timezone.min.js') }}"></script>
-<script src="{{ asset('lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js') }}"></script>
-
-</body>
-</html>
+                    <script src="{{ asset('js/app.js') }}"></script>
+@endsection
